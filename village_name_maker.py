ca0fc1a8f128daf3a5d0fa9e11d0b2d0124f1e39
matt <mrallinson@gmail.com>
2019-10-06 22:21:21 +0300

    adds first example

--- village_name_maker.py
+++ village_name_maker.py
@@ -3,10 +3,24 @@ prints to terminal using temp of 1
 '''
 
 from textgenrnn import textgenrnn
+import argparse
 
-textgen = textgenrnn('data/villages_2e.hdf5')
-villages = textgen.generate(10, temperature=1, return_as_list=True)
+parser = argparse.ArgumentParser(description="what's going on 'ere?")
 
-for village in villages:
-	print(village)
- 
+parser.add_argument('-n', '--number', type=int, default=100),
+parser.add_argument('-m', '--model', default='weights/villages_2e.hdf5'),
+parser.add_argument('-t', '--temperature', type=int, default=1)
+parser.add_argument('-o', '--output', default='ai_villages')
+
+args=parser.parse_args()
+
+textgen = textgenrnn(args.model)
+villages = textgen.generate(args.number, temperature=args.temperature,
+							return_as_list=True)
+
+filename = args.output+'_temp'+str(args.temperature)+'.txt'
+
+for ai_village in villages:
+	print(ai_village)
+	with open(filename ,'a') as village_file:
+				village_file.write(ai_village +'\n')
