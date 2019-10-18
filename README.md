18f09cf76eb840a9c1fd18ecb9fd816a22984f36
Allinson Matthew <mrallinson@gmail.com>
2019-10-18 23:35:11 +0300

    adds names with different temps

--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # Aim of project
-Neural network that produces 10 english village names
+Neural network that produces fictional english village names. Trained on a list of all the 16,599 english village names on Wikipedia.
+Eventually I guess I'll save all this in a nicely formatted ipython notebook. 
 
-## Village Name
-
-Run `python3 village_name_maker.py` for some fun english village name-based fun.
+## Village Name Maker
+Run `python3 village_name_maker.py` for some fun english village name-based fun. 
 
 ## Village finder
-An extrememly messy script that once upon a time produced `english_village_names.txt` by scraping wikipedia but since the number of English villages is probably staying the same, I'd leave this be.
+An extrememly messy script that once upon a time produced `english_village_names.txt` by scraping wikipedia. Might be adaptable for future use as a general purpose wikipedia scraper.  
 
 ## More functionality
 See the docs for [textgenrnn](https://github.com/minimaxir/textgenrnn/blob/master/docs/textgenrnn-demo.ipynb) for more advice about how to get this to work properly.
--- village_name_maker.py
+++ village_name_maker.py
@@ -1,4 +1,4 @@
-'''Makes 10 village names using pretrained model
+'''Makes village names using pretrained model
 prints to terminal using temp of 1
 '''
 
@@ -8,19 +8,32 @@ import argparse
 parser = argparse.ArgumentParser(description="what's going on 'ere?")
 
 parser.add_argument('-n', '--number', type=int, default=100),
-parser.add_argument('-m', '--model', default='weights/villages_2e.hdf5'),
-parser.add_argument('-t', '--temperature', type=int, default=1)
+parser.add_argument('-m', '--model', default='weights/villages_4e.hdf5')
+parser.add_argument('-t', '--temperature', type=float, default=1.0)
 parser.add_argument('-o', '--output', default='ai_villages')
+parser.add_argument('-r', '--reference', 
+						default='data/english_village_names.txt')
 
+#Initialises
 args=parser.parse_args()
+output_filename = 'output/'+args.output+'_temp'+str(args.temperature)+'.txt'
+real_villages = [village.strip() for village in
+					open(args.reference).readlines()]
 
+#Loads Neural Net and generates names
 textgen = textgenrnn(args.model)
 villages = textgen.generate(args.number, temperature=args.temperature,
 							return_as_list=True)
 
-filename = 'output/'_args.output+'_temp'+str(args.temperature)+'.txt'
+#Saves Names
+for ai_village in sorted(villages):
+	if ai_village not in real_villages:
+		print(ai_village)
+		with open(output_filename ,'a') as village_file:
+					village_file.write(ai_village +'\n')
 
-for ai_village in villages:
-	print(ai_village)
-	with open(filename ,'a') as village_file:
-				village_file.write(ai_village +'\n')
+#Tidies name file
+out = sorted([v for v in open(output_filename).readlines()])
+with open(output_filename,'w') as outfile:
+	for o in out:
+		outfile.write(o)
